b9b055c8f7d6a625d3ec62f923101e5f02718a3f
Guwan <Guwan@Guwan.com>
2025-01-03 18:05:06 +0800

    Site updated: 2025-01-03 18:05:06

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1735200904093";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1735898692593";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
